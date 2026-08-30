4105648a233761f05498bf6c5b27e8256dd57531
yossibank <yossibank@yahoo.co.jp>
2026-08-30 21:47:41 +0900

    Release v0.3.0

--- Package.swift
+++ Package.swift
@@ -1,9 +1,5 @@
 // swift-tools-version: 6.0
-// url と checksum は共通コアの publish 時に書き換える。手で編集しないこと。
-//
-// XCFramework は GitHub Releases ではなく GitHub Packages(Maven) に置く。
-// private リポジトリのリリースアセットは api.github.com 経由でしか取得できず、
-// その URL は拡張子が無いため SPM の binaryTarget が受け付けないため。
+// このファイルは release.sh が生成する。手で編集しないこと。
 import PackageDescription
 
 let package = Package(
@@ -14,8 +10,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "Shared",
-            url: "https://maven.pkg.github.com/yossibank/kmp-app-template/com/yossibank/shared-xcframework/0.2.0/shared-xcframework-0.2.0.zip",
-            checksum: "b7962b70ae7a9560f48060a554784be95331150abfd81e75cf160cd285aeaa1d"
+            url: "https://api.github.com/repos/yossibank/kmp-app-template/releases/assets/536608783.zip",
+            checksum: "a36ed3d8d13a184fbed19b41f7037df7eca8fdf599cc70b9ff22945f2cc8b744"
         )
     ]
 )
